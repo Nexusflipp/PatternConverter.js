d5c15a434939bb4ad721dcf1d7bb7db03134c2b3
Nexu$flipp <81493734+Nexusflipp@users.noreply.github.com>
2023-04-03 10:50:48 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# PatternConverter.js
+
+<br>
+
+<p align="center"> 
+     <a href="#" target="_blank"> <img src="https://img.shields.io/tokei/lines/github/Nexusflipp/PatternConverter.js"/> </a>
+    <a href="#" target="_blank"> <img src="https://img.shields.io/github/issues/Nexusflipp/PatternConverter.js"/> </a>
+    <a href="#" target="_blank"> <img src="https://img.shields.io/github/languages/top/Nexusflipp/PatternConverter.js"/> </a> 
+    <a href="#" target="_blank"> <img src="https://img.shields.io/github/languages/count/Nexusflipp/PatternConverter.js"/> </a> 
+    <a href="#" target="_blank"> <img src="https://img.shields.io/github/last-commit/Nexusflipp/PatternConverter.js"/> </a> 
+    <a href="#" target="_blank"> <img src="https://img.shields.io/github/repo-size/Nexusflipp/PatternConverter.js"/> </a> 
+    <a href="#" target="_blank"> <img src="https://img.shields.io/github/languages/code-size/Nexusflipp/PatternConverter.js"/> </a> 
+</p>
+
+<br>
+
+Barebones online byte pattern converter.<br>
+<b>A more polished and ready to use version can be found at: <a href="https://nexusflipp.dev/pattern/converter">nexusflipp.dev/pattern/converter</a></b><br>
+This code also shows the different ways replace can be used, for more information visit: <a href="https://www.regextester.com">Regex Tester</a><br>
+For bug reports or code improvements dm me: <a href="https://nexusflipp.dev/contact">Contact</a><br>
+
+<br>
+
+## Features
+
+<br>
+
+<ul>
+  <li>Mirror of the console messages and errors, with autoscroll.</li>
+  <li>Convert IDA style patterns to code style patterns.</li>
+  <li>Convert code style patterns to IDA style patterns.</li>
+  <li>Safe from cross-site scripting attacks.</li>
+  <li>Toggle IDA style without whitespaces.</li>
+  <li>Toggle IDA style with double ?</li>
+  <li>Toggle IDA style with *.</li>
+  <li>Toggle IDA style with double *.</li>
+  <li>Toggle console debug messages.</li>
+  <li>Toggle mask generation.</li>
+</ul>
+
+<br>
+
+## Credits
+
+<br>
+
+<ul>
+     <li><a href="https://www.danstools.com/">@danstools00</a> for his regex tool.</li>
+     <li><a href="https://developer.mozilla.org/">Mozilla</a> for their javascript documentation.</li>
+</ul>
---
+++ patternconverter.js
@@ -0,0 +1,426 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  INPUT
+
+
+function SanitizeInput(Input) 
+{
+    if (Input.length > 0)
+    {
+        if (Input.match(/^[a-zA-Z0-9\s\\?*]+$/g))
+            return Input;
+        else
+        {
+            alert("Invalid input!");
+            return "";
+        }
+    }
+    else
+        return "";
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  IDA TO CODE
+
+
+const Elements = 
+{
+    Debug:      document.getElementById('debug'),
+    DropDown:   document.getElementById('dropdown'),
+    CheckBoxM:  document.getElementById('mask'),
+    CheckBoxW:  document.getElementById('white'),
+};
+
+for (const Key in Elements) 
+{
+    if (Object.hasOwnProperty.call(Elements, Key)) {
+      const Element = Elements[Key];
+      Element.addEventListener('change', () => { ToCode(); });
+    }
+}
+
+function ToCode()
+{
+    var Mask            = document.getElementById('outmask');
+    var outpat          = document.getElementById('outcode');
+    var codePattern     = SanitizeInput(document.getElementById('inida').value.trim());
+    const SelectedValue = Elements.DropDown.value;
+
+    if (codePattern.length <= 0)
+        return;
+
+    switch (SelectedValue) 
+    {
+        case "normal":
+        {
+            if (Elements.CheckBoxW.checked)
+            {
+                codePattern = codePattern.replace(/\u003f/g, "\u003f\u003f");                                   //  replace every ? with ??
+                codePattern = codePattern.replace(/[^\dA-Z\u003f]/g, '').replace(/(.{2})/g, '$1 ').trim();      //  insert a space each 2 characters in our list
+                codePattern = codePattern.replace(/\u003f\u003f/g, "\u003f");                                   //  replace very ?? with ?
+            }
+
+            if (Elements.CheckBoxM.checked) 
+            {
+                //  show mask div
+                Mask.style.display = 'block';
+        
+                var outmask = codePattern.slice();
+                outmask = outmask.replace(/\b(?!\u003f)\b\S+/g, "x");                                                   //  replace every word with x, except all starting with ?
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                    //  mask
+                outmask = outmask.replace(/\s/g, '');                                                           //  remove every space
+                //  I use textContent here to prevent XSS attacks
+                Mask.textContent = outmask; 
+
+                //  pattern
+                codePattern = codePattern.replace(/\u003f/g, '00');
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+                outpat.textContent = codePattern;
+
+                if (Elements.Debug.checked && outpat.length != 0) 
+                    console.log(outpat.textContent);
+
+                if (Elements.Debug.checked && outmask.length != 0)
+                    console.log(outmask); 
+            }
+            else
+            {
+                //  hide mask div
+                Mask.style.display = 'none';
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                codePattern = codePattern.replace(/\u003f/g, '00');                                                 //  replace every ? with 00
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+            
+                //  I use textContent here to prevent XSS attacks
+                outpat.textContent = codePattern;
+            
+                if (Elements.Debug.checked && outpat.length != 0)
+                    console.log(outpat.textContent); 
+            }
+        }
+        break;
+
+        case "double?":
+        {
+            if (Elements.CheckBoxW.checked)
+                codePattern = codePattern.replace(/[^\dA-Z\u003f]/g, '').replace(/(.{2})/g, '$1 ').trim();      //  insert a space each 2 characters in our list
+
+            if (Elements.CheckBoxM.checked) 
+            {
+                //  show mask div
+                Mask.style.display = 'block';
+        
+                var outmask = codePattern.slice();
+                outmask = outmask.replace(/\b(?!\u003f)\b\S+/g, "x");                                                   //  replace every word with x, except all starting with ?
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                //  mask
+                outmask= outmask.replace(/\u003f\u003f/g, "\u003f");                                            //  replace very ?? with ?
+                outmask = outmask.replace(/\s/g, '');                                                           //  remove all spaces
+                //  I use textContent here to prevent XSS attacks
+                Mask.textContent = outmask; 
+
+                //  pattern
+                codePattern = codePattern.replace(/\u003f\u003f/g, "00"); 
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+                outpat.textContent = codePattern;
+
+                if (Elements.Debug.checked && outpat.length != 0) 
+                    console.log(outpat.textContent);
+
+                if (Elements.Debug.checked && outmask.length != 0)
+                    console.log(outmask);
+            }
+            else
+            {
+                //  hide mask div
+                Mask.style.display = 'none';
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                codePattern = codePattern.replace(/\u003f\u003f/g, "00"); 
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+
+                //  I use textContent here to prevent XSS attacks
+                outpat.textContent = codePattern;
+
+                if (Elements.Debug.checked && outpat.length != 0)
+                    console.log(outpat.textContent); 
+            }
+        }
+        break;
+
+        case "asterisk":
+        {
+            if (Elements.CheckBoxW.checked)
+            {
+                codePattern = codePattern.replace(/\u002a/g, "\u002a\u002a");                                   //  replace every ? with ??
+                codePattern = codePattern.replace(/[^\dA-Z\u002a]/g, '').replace(/(.{2})/g, '$1 ').trim();      //  insert a space each 2 characters in our list
+                codePattern = codePattern.replace(/\u002a\u002a/g, "\u002a");                                   //  replace very ?? with ?
+            }
+
+            if (Elements.CheckBoxM.checked) 
+            {
+                //  show mask div
+                Mask.style.display = 'block';
+        
+                var outmask = codePattern.slice();
+                outmask = outmask.replace(/\b(?!\u002a)\b\S+/g, "x");                                                   //  replace every word with x, except all starting with ?
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                outmask = outmask.replace(/\*/g, "?");
+
+                    //  mask
+                outmask = outmask.replace(/\s/g, '');                                                           //  remove every space
+                //  I use textContent here to prevent XSS attacks
+                Mask.textContent = outmask; 
+
+                //  pattern
+                codePattern = codePattern.replace(/\u002a/g, '00');
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+                outpat.textContent = codePattern;
+
+                if (Elements.Debug.checked && outpat.length != 0) 
+                    console.log(outpat.textContent);
+
+                if (Elements.Debug.checked && outmask.length != 0)
+                    console.log(outmask); 
+            }
+            else
+            {
+                //  hide mask div
+                Mask.style.display = 'none';
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                codePattern = codePattern.replace(/\u002a/g, '00');                                                 //  replace every ? with 00
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+            
+                //  I use textContent here to prevent XSS attacks
+                outpat.textContent = codePattern;
+            
+                if (Elements.Debug.checked && outpat.length != 0)
+                    console.log(outpat.textContent); 
+            }
+        }
+        break;
+
+        case "double*":
+        {
+            if (Elements.CheckBoxW.checked)
+                codePattern = codePattern.replace(/[^\dA-Z\u002a]/g, '').replace(/(.{2})/g, '$1 ').trim();      //  insert a space each 2 characters in our list
+
+            if (Elements.CheckBoxM.checked) 
+            {
+                //  show mask div
+                Mask.style.display = 'block';
+        
+                var outmask = codePattern.slice();
+                outmask = outmask.replace(/\b(?!\u002a)\b\S+/g, "x");                                                   //  replace every word with x, except all starting with ?
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                //  mask
+                outmask = outmask.replace(/\u002a\u002a/g, "\u003f");                                            //  replace very ** with ?
+                outmask = outmask.replace(/\s/g, '');                                                           //  remove all spaces
+                //  I use textContent here to prevent XSS attacks
+                Mask.textContent = outmask; 
+
+                //  pattern
+                codePattern = codePattern.replace(/\u002a\u002a/g, "00"); 
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+                outpat.textContent = codePattern;
+
+                if (Elements.Debug.checked && outpat.length != 0) 
+                    console.log(outpat.textContent);
+
+                if (Elements.Debug.checked && outmask.length != 0)
+                    console.log(outmask);
+            }
+            else
+            {
+                //  hide mask div
+                Mask.style.display = 'none';
+
+                if (codePattern.length != 0)
+                    codePattern = codePattern.split(' ').map(s => '\u005cx' + s).join(' ');
+
+                codePattern = codePattern.replace(/\u002a\u002a/g, "00"); 
+
+                codePattern = codePattern.replace(/\s/g, '');                                                           //  remove every space
+
+                //  I use textContent here to prevent XSS attacks
+                outpat.textContent = codePattern;
+
+                if (Elements.Debug.checked && outpat.length != 0)
+                    console.log(outpat.textContent); 
+            }
+        }
+        break;
+
+        default:
+        break;
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  CODE TO IDA
+
+
+const Elements2 = 
+{
+    Debug:      document.getElementById('debug2'),
+    DropDown:   document.getElementById('dropdown2'),
+    CheckBoxW:  document.getElementById('white2'),
+};
+
+for (const Key in Elements2) 
+{
+    if (Object.hasOwnProperty.call(Elements2, Key)) {
+      const Element = Elements2[Key];
+      Element.addEventListener('change', () => { ToIda(); });
+    }
+}
+
+function ToIda()
+{
+    const Debug     = document.getElementById('debug2');
+    var idaPattern  = SanitizeInput(document.getElementById('incode').value);
+    var Out         = document.getElementById('outida');
+
+    if (idaPattern.length <= 0)
+        return;
+
+    const SelectedValue = Elements2.DropDown.value;
+    switch (SelectedValue) {
+        
+        case "normal":
+        {
+            idaPattern = idaPattern.replace(/00/g, '?');                                                            //  replace every 00 with ?
+
+            //  if no whitespaces is toggled
+            if (Elements2.CheckBoxW.checked)
+                idaPattern = idaPattern.replace(/\u005cx/g, '');                                                    //  erase every \x
+            else
+                idaPattern = idaPattern.replace(/\u005cx/g, ' ');                                                   //  replace every \x with a space
+        
+            //  I use textContent here to prevent XSS attacks
+            Out.textContent = idaPattern.trim();                                                                    //  trim and output
+        
+            if (Debug.checked && Out.length != 0)
+                console.log(Out.textContent); 
+        }
+        break;
+
+        case "double?":
+        {
+            idaPattern = idaPattern.replace(/00/g, '??');                                                           //  replace every 00 with ??
+
+            //  if no whitespaces is toggled
+            if (Elements2.CheckBoxW.checked)
+                idaPattern = idaPattern.replace(/\u005cx/g, '');                                                    //  erase every \x
+            else
+                idaPattern = idaPattern.replace(/\u005cx/g, ' ');                                                   //  replace every \x with a space
+                    
+            //  I use textContent here to prevent XSS attacks
+            Out.textContent = idaPattern.trim();                                                                    //  trim and output
+            
+            if (Debug.checked && Out.length != 0)
+                console.log(Out.textContent); 
+        }
+        break;
+
+        case "asterisk":
+        {
+            idaPattern = idaPattern.replace(/00/g, '*');                                                            //  replace every 00 with ?
+
+            //  if no whitespaces is toggled
+            if (Elements2.CheckBoxW.checked)
+                idaPattern = idaPattern.replace(/\u005cx/g, '');                                                    //  erase every \x
+            else
+                idaPattern = idaPattern.replace(/\u005cx/g, ' ');                                                   //  replace every \x with a space
+        
+            //  I use textContent here to prevent XSS attacks
+            Out.textContent = idaPattern.trim();                                                                    //  trim and output
+        
+            if (Debug.checked && Out.length != 0)
+                console.log(Out.textContent); 
+        }
+        break;
+
+        case "double*":
+        {
+            idaPattern = idaPattern.replace(/00/g, '**');                                                           //  replace every 00 with ??
+
+            //  if no whitespaces is toggled
+            if (Elements2.CheckBoxW.checked)
+                idaPattern = idaPattern.replace(/\u005cx/g, '');                                                    //  erase every \x
+            else
+                idaPattern = idaPattern.replace(/\u005cx/g, ' ');                                                   //  replace every \x with a space
+                    
+            //  I use textContent here to prevent XSS attacks
+            Out.textContent = idaPattern.trim();                                                                    //  trim and output
+            
+            if (Debug.checked && Out.length != 0)
+                console.log(Out.textContent); 
+        }
+        break;
+    
+        default:
+            break;
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  DEBUG CONSOLE
+
+
+const console = document.getElementById('c');
+
+//  display errors
+window.onerror = function(error)
+{
+    console.textContent += '[ERR]: ' + error.toString() + '\r\n';  
+    console.scrollIntoView({behavior: "smooth", block: "end"}); 
+}
+
+//  display logs
+console.log = function(message) 
+{
+    console.textContent += '[MSG]: ' + message + '\r\n';
+    console.scrollIntoView({behavior: "smooth", block: "end"});
+}
+
+//  test console log
+function TestLog()
+{
+    console.log('This is a test!');
+}
+
+function ClearConsole()
+{
+    console.replaceChildren();
+}
